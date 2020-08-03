3132251c52593a134a537f28a971234332048bf8
4z7l <7d4z7l@naver.com>
2020-08-03 18:23:40 +0900

    Add click listener on delete button, search button

--- app/src/main/java/com/wearweather/WeatherPagerAdpater.java
+++ app/src/main/java/com/wearweather/WeatherPagerAdpater.java
@@ -1,19 +1,45 @@
 package com.wearweather;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 
+import java.util.ArrayList;
+
 public class WeatherPagerAdpater extends FragmentStatePagerAdapter {
+    public static ArrayList<WeatherFragment> weatherFragmentArrayList = new ArrayList<>();
 
     public WeatherPagerAdpater(@NonNull FragmentManager fm, int behavior) {
         super(fm, behavior);
     }
 
+    public void initFragment() {
+        addWeatherFragment();
+        addWeatherFragment();
+    }
+
+    public void addWeatherFragment() {
+        weatherFragmentArrayList.add(new WeatherFragment());
+        Log.e("SEULGI NOW FRAGMENT",String.valueOf(weatherFragmentArrayList.size()));
+        notifyDataSetChanged();
+    }
+    public void deleteWeatherFragment(int position){
+        Log.e("SEULGI DELETE FRAGMENT",String.valueOf(position));
+        weatherFragmentArrayList.remove(position);
+        Log.e("SEULGI NOW FRAGMENT",String.valueOf(weatherFragmentArrayList.size()));
+        notifyDataSetChanged();
+    }
+    public int getWeatherFragmentSize() {
+        return weatherFragmentArrayList.size();
+    }
     @NonNull
     @Override
     public Fragment getItem(int position) {
+        return weatherFragmentArrayList.get(position);
+        /*
         switch (position){
             case 0:
                 WeatherFragment fragment1 = new WeatherFragment();
@@ -24,10 +50,11 @@ public class WeatherPagerAdpater extends FragmentStatePagerAdapter {
             default:
                 return null;
         }
+        */
     }
 
     @Override
     public int getCount() {
-        return 2;
+        return getWeatherFragmentSize();
     }
 }
