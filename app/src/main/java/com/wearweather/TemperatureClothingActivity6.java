6e8f8b3c2253aefc8e580760cdb477a0b4430a7a
shinbian11 <shinbian11@naver.com>
2020-08-06 16:00:53 +0900

    8.6 ji_hwan kim commit

---
+++ app/src/main/java/com/wearweather/TemperatureClothingActivity6.java
@@ -0,0 +1,14 @@
+package com.wearweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TemperatureClothingActivity6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temperature_clothing6);
+    }
+}
