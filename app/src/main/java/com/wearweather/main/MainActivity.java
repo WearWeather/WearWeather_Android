dd64add2fc3b389a2cab38a036c2b13115d5fa33
Seulgi Kim <7d4z7l@gmail.com>
2020-08-10 21:09:12 +0900

    ADD New Main Activity

---
+++ app/src/main/java/com/wearweather/main/MainActivity.java
@@ -0,0 +1,54 @@
+package com.wearweather.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.tabs.TabLayout;
+import com.wearweather.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private MainTabPagerAdapter pagerAdpater;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* tab layout */
+        tabLayout = (TabLayout) findViewById(R.id.main_tab_layout);
+        pagerAdpater = new MainTabPagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        pagerAdpater.initFragment();
+        for(int i=0;i<pagerAdpater.getWeatherFragmentSize();i++){
+            tabLayout.addTab(tabLayout.newTab());
+        }
+
+        viewPager = (ViewPager) findViewById(R.id.main_tab_viewpager);
+        viewPager.setAdapter(pagerAdpater);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                pagerAdpater.notifyDataSetChanged();
+                viewPager.setCurrentItem(tab.getPosition());
+                Log.e("SEULGI TAB SELECTED",String.valueOf(tab.getPosition()));
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/wearweather/main/MainTabPagerAdapter.java
@@ -0,0 +1,64 @@
+package com.wearweather.main;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import com.wearweather.WeatherFragment;
+
+import java.util.ArrayList;
+
+public class MainTabPagerAdapter extends FragmentStatePagerAdapter {
+    public static ArrayList<MainWeatherFragment> weatherFragmentArrayList = new ArrayList<>();
+
+    public MainTabPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    public void initFragment() {
+        if(getWeatherFragmentSize()==0){
+            addWeatherFragment();
+            addWeatherFragment();
+        }
+    }
+
+    public void addWeatherFragment() {
+        weatherFragmentArrayList.add(new MainWeatherFragment());
+        Log.e("SEULGI NOW FRAGMENT",String.valueOf(weatherFragmentArrayList.size()));
+        notifyDataSetChanged();
+    }
+    public void deleteWeatherFragment(int position){
+        Log.e("SEULGI DELETE FRAGMENT",String.valueOf(position));
+        weatherFragmentArrayList.remove(position);
+        Log.e("SEULGI NOW FRAGMENT",String.valueOf(weatherFragmentArrayList.size()));
+        notifyDataSetChanged();
+    }
+    public int getWeatherFragmentSize() {
+        return weatherFragmentArrayList.size();
+    }
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return weatherFragmentArrayList.get(position);
+        /*
+        switch (position){
+            case 0:
+                WeatherFragment fragment1 = new WeatherFragment();
+                return fragment1;
+            case 1:
+                WeatherFragment fragment2 = new WeatherFragment();
+                return fragment2;
+            default:
+                return null;
+        }
+        */
+    }
+
+    @Override
+    public int getCount() {
+        return getWeatherFragmentSize();
+    }
+}
