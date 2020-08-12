23b635b3526b8e16268d702cef155e270426d72f
JihoKevin <jhshin925@gmail.com>
2020-08-12 15:01:39 +0900

    revised

--- app/src/main/java/com/wearweather/TemperatureClothingActivity.java
+++ app/src/main/java/com/wearweather/TemperatureClothingActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-
-
 public class TemperatureClothingActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/com/wearweather/TemperatureClothingActivity2.java
+++ app/src/main/java/com/wearweather/TemperatureClothingActivity2.java
@@ -2,13 +2,36 @@ package com.wearweather;
 /* 5°C ~ 9°C 이하 일 경우 추천해주는 액티비티 */
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class TemperatureClothingActivity2 extends AppCompatActivity {
+
+    ViewGroup tokboki, shoes, thickCoat, neat, scarf, leather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temperature_clothing2);
+
+        tokboki = (ViewGroup) findViewById(R.id.layout_tokboki);
+
+        tokboki.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String url = "http://store.musinsa.com/app/product/detail/1196926/0";
+
+                Intent intent = new Intent(getApplicationContext() , ClothesClickWebView.class);
+                intent.putExtra("LinkTokboki", url);
+                startActivity(intent);
+            }
+        });
     }
 }
