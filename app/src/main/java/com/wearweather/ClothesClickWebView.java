25ed48e3f00cf0ab2c7b9bfafea8406664019640
JihoKevin <jhshin925@gmail.com>
2020-08-12 13:57:00 +0900

    added webView for shopping

---
+++ app/src/main/java/com/wearweather/ClothesClickWebView.java
@@ -0,0 +1,14 @@
+package com.wearweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClothesClickWebView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clothes_click_web_view);
+    }
+}
