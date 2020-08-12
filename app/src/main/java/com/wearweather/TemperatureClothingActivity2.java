730cab01f74ddf75ff601abe11d5e2a6aa272a54
JihoKevin <jhshin925@gmail.com>
2020-08-12 15:43:39 +0900

    revised

--- app/src/main/java/com/wearweather/TemperatureClothingActivity2.java
+++ app/src/main/java/com/wearweather/TemperatureClothingActivity2.java
@@ -14,6 +14,8 @@ import java.net.URL;
 public class TemperatureClothingActivity2 extends AppCompatActivity {
 
     ViewGroup tokboki, shoes, thickCoat, neat, scarf, leather;
+    String url;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +23,48 @@ public class TemperatureClothingActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_temperature_clothing2);
 
         tokboki = (ViewGroup) findViewById(R.id.layout_tokboki);
+        shoes = (ViewGroup) findViewById(R.id.layout_winterShoes);
+        thickCoat = (ViewGroup) findViewById(R.id.layout_thickCoat);
+        neat = (ViewGroup) findViewById(R.id.layout_neat);
+        scarf = (ViewGroup) findViewById(R.id.layout_scarf);
+        leather = (ViewGroup) findViewById(R.id.layout_leather);
 
-        tokboki.setOnClickListener(new View.OnClickListener() {
+        View.OnClickListener listener = new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-
-                String url = "http://store.musinsa.com/app/product/detail/1196926/0";
-
-                Intent intent = new Intent(getApplicationContext(), ClothesClickWebView.class);
-                intent.putExtra("LinkTokboki", url);
-                startActivity(intent);
+                switch (view.getId()){
+                    case R.id.layout_tokboki:
+                        url = "https://store.musinsa.com/app/product/detail/1196926/0";
+                        intent = new Intent(getApplicationContext(), ClothesClickWebView.class);
+                        intent.putExtra("link", url);
+                        startActivity(intent);
+                    case R.id.layout_winterShoes:
+                        url = "https://store.musinsa.com/app/product/detail/1269289/0";
+                        intent = new Intent(getApplicationContext(), ClothesClickWebView.class);
+                        intent.putExtra("link", url);
+                        startActivity(intent);
+                    case R.id.layout_thickCoat:
+                        url = "https://store.musinsa.com/app/product/detail/1180105/0";
+                        intent = new Intent(getApplicationContext(), ClothesClickWebView.class);
+                        intent.putExtra("link", url);
+                        startActivity(intent);
+                    case R.id.layout_neat:
+                        url = "https://store.musinsa.com/app/product/detail/1435001/0";
+                        intent = new Intent(getApplicationContext(), ClothesClickWebView.class);
+                        intent.putExtra("link", url);
+                        startActivity(intent);
+                    case R.id.layout_scarf:
+                        url = "https://store.musinsa.com/app/product/detail/897557/0";
+                        intent = new Intent(getApplicationContext(), ClothesClickWebView.class);
+                        intent.putExtra("link", url);
+                        startActivity(intent);
+                    case R.id.layout_leather:
+                        url = "https://store.musinsa.com/app/product/detail/475009/0";
+                        intent = new Intent(getApplicationContext(), ClothesClickWebView.class);
+                        intent.putExtra("link", url);
+                        startActivity(intent);
+                }
             }
-        });
+        };
     }
 }
