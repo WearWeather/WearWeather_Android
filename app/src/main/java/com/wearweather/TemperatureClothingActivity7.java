5bad9abfb42b887b24ce83470feb2534bbb9d718
JihoKevin <jhshin925@gmail.com>
2020-08-23 14:36:01 +0900

    revised clothing tap's design.

--- app/src/main/java/com/wearweather/TemperatureClothingActivity7.java
+++ app/src/main/java/com/wearweather/TemperatureClothingActivity7.java
@@ -21,7 +21,7 @@ public class TemperatureClothingActivity7 extends AppCompatActivity {
 
         String name = intent.getStringExtra("temperature");
 
-        tx9.setText(name);
+        tx9.setText(name + "°C");
 
         LinearLayout layout1 = (LinearLayout) findViewById(R.id.layout1);
         layout1.setOnClickListener(new View.OnClickListener() {
