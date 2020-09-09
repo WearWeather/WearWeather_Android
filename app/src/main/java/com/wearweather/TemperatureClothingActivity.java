e2ba0d392e04d65ac9c4b2d77697cf86d28cd93b
shinbian11 <shinbian11@gmail.com>
2020-09-09 14:50:55 +0900

    Merge branch 'master' of https://github.com/4z7l/WearWeather
     Conflicts:
    	app/src/main/AndroidManifest.xml
    	app/src/main/res/layout/activity_clothes_click_web_view.xml

--- app/src/main/java/com/wearweather/TemperatureClothingActivity.java
+++ app/src/main/java/com/wearweather/TemperatureClothingActivity.java
@@ -44,7 +44,7 @@ public class TemperatureClothingActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent  = new Intent(getApplicationContext(), ClothesClickWebView.class);
-                intent.putExtra("link", "https://store.musinsa.com/app/product/detail/1515905/0");
+                intent.putExtra("link", "https://www.meosidda.com/product/%EA%B5%BF%EC%B4%88%EC%9D%B4%EC%8A%A4-%EB%A7%88%EC%9D%B4%ED%81%AC%EB%A1%9C-%EB%A1%B1%ED%8C%A8%EB%94%A9/30369/category/76/display/1/");
                 startActivity(intent);
             }
         });
@@ -54,7 +54,7 @@ public class TemperatureClothingActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent  = new Intent(getApplicationContext(), ClothesClickWebView.class);
-                intent.putExtra("link", "https://store.musinsa.com/app/product/detail/1139473/0");
+                intent.putExtra("link", "https://www.meosidda.com/product/%EC%B6%94%EC%9C%84%EC%95%BC-%EB%AC%BC%EB%9F%AC%EB%82%98%EB%9D%BC-%EA%B8%B0%EB%AA%A8%ED%9B%84%EB%93%9C%ED%8B%B0/29974/category/30/display/1/");
                 startActivity(intent);
             }
         });
@@ -64,7 +64,7 @@ public class TemperatureClothingActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent  = new Intent(getApplicationContext(), ClothesClickWebView.class);
-                intent.putExtra("link", "https://store.musinsa.com/app/product/detail/1180105/0");
+                intent.putExtra("link", "https://www.meosidda.com/product/%EC%98%86-%EB%B0%91%EB%8B%A8%ED%8A%B8%EC%9E%84-%EC%98%A4%EB%B2%84%ED%95%8F-%EB%A1%B1-%EC%BD%94%ED%8A%B8/36754/category/78/display/1/");
                 startActivity(intent);
             }
         });
@@ -74,7 +74,7 @@ public class TemperatureClothingActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent  = new Intent(getApplicationContext(), ClothesClickWebView.class);
-                intent.putExtra("link", "https://store.musinsa.com/app/product/detail/1435001/0");
+                intent.putExtra("link", "https://www.meosidda.com/product/%EB%A0%88%EC%9D%B4-%EB%B0%98%ED%8F%B4%EB%9D%BC-%ED%8B%B0%EC%85%94%EC%B8%A0/36378/category/90/display/1/");
                 startActivity(intent);
             }
         });
@@ -84,7 +84,7 @@ public class TemperatureClothingActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent  = new Intent(getApplicationContext(), ClothesClickWebView.class);
-                intent.putExtra("link", "https://store.musinsa.com/app/product/detail/1269289/0");
+                intent.putExtra("link", "https://www.meosidda.com/product/4cm-%EB%A0%88%EC%9D%B4%EC%8A%A4%EC%97%85-%EC%9B%8C%EC%BB%A4/36109/category/87/display/1/");
                 startActivity(intent);
             }
         });
@@ -94,7 +94,7 @@ public class TemperatureClothingActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent  = new Intent(getApplicationContext(), ClothesClickWebView.class);
-                intent.putExtra("link", "https://store.musinsa.com/app/product/detail/897557/0");
+                intent.putExtra("link", "https://www.meosidda.com/product/%EA%B3%A8%EC%A7%80-%EC%9A%B8-%EB%AA%A9%EB%8F%84%EB%A6%AC/34342/category/1560/display/1/");
                 startActivity(intent);
             }
         });
