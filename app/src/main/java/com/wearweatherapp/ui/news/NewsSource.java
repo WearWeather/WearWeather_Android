423fa27a4f6bede69e402a9d034b4bf38c97b90e
JihoKevin <jhshin925@gmail.com>
2021-03-16 19:19:24 +0900

    뉴스 정책 수정

---
+++ app/src/main/java/com/wearweatherapp/ui/news/NewsSource.java
@@ -0,0 +1,18 @@
+package com.wearweatherapp.ui.news;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.wearweatherapp.R;
+
+public class NewsSource extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_source);
+
+
+    }
+}
